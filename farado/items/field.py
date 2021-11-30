9e82fc895b50a1d3cee7f777767a1607e61e8884
Ostapenko Alexey <dolu.bl@gmail.com>
2021-11-30 09:01:49 +0300

    [items] The basic elements of the subject area have been added.

---
+++ farado/items/field.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Field():
+    def __init__( self
+                , value
+                , fieldkind_id = None
+                , issue_id = None
+                ):
+        self.id = None
+        self.value = value
+        self.fieldkind_id = fieldkind_id
+        self.issue_id = issue_id
+
+    def __repr__(self):
+        return str(
+            f'''<Field(id='{ self.id
+                }',\n issue_id='{ self.issue_id
+                }',\n fieldkind_id='{ self.fieldkind_id
+                }',\n value='{ self.value
+                }')>'''
+            )
