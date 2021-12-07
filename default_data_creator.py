4cc63fcc5992d44d5b9626edbdd2a1afb670e4a4
Ostapenko Alexey <dolu.bl@gmail.com>
2021-12-07 23:17:48 +0300

    Config for default data creator

--- default_data_creator.py
+++ default_data_creator.py
@@ -3,9 +3,12 @@
 
 from farado.items.user import User
 from farado.items.meta_item_manager import MetaItemManager
+from farado.config import farado_config
+
+
 
 if __name__ == '__main__':
-    manager = MetaItemManager()
+    manager = MetaItemManager(farado_config['database']['connection_string'])
     user = User(
         'admin',
         'John',
